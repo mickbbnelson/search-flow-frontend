8bfc7883c2cc37ddd34189931585f0b498e3d6f4
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-04-28 15:52:05 -0600

    Add image and css to product cards

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,7 +1,11 @@
 export default function ProductCard(props){
     return(
-        <div>
-            <span>{props.product.name}</span>
+        <div className="card">
+            <img src={props.product.image} width="200" height="200" className="cardImage"/>
+            {/* <span>{props.product.name}</span> */}
+            <div className="cardContainer">
+                <h4>{props.product.name} - ${props.product.price}</h4>
+            </div>
         </div>
     )
 }
