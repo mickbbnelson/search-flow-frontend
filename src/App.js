380b57bbdaf8d03f6aaeac854ba1627d38761685
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-05-04 13:32:09 -0600

    Add footer component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Products from './ProductsContainer'
 import ProductSelect from './ProductSelect';
 import AddressForm from './AddressForm'
 import NavBar from './NavBar';
+import Footer from './Footer';
 
 function App() {
   return (
+    <div>
     <div className="App">
       <NavBar />
       <AddressForm />
       <ProductSelect />
       <Products />
+      <Footer />
+    </div>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <ul>
+                <li><a href="">Los Angeles</a></li>
+                <li><a href="">Orange County</a></li>
+                <li><a href="">San Francisco</a></li>
+                <li><a href="">San Jose</a></li>
+                <li><a href="">East Bay</a></li>
+                <li><a href="">Sacramento</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
